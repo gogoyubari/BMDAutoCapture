38fa072fcfdbb5eb8be9c247137629c0b9c117d5
gogoyubari <1097517+gogoyubari@users.noreply.github.com>
2021-05-17 12:04:14 +0900

    解析時間の上限をマイクロ秒で指定

--- Capture.ps1
+++ Capture.ps1
@@ -39,12 +39,10 @@ $ps = Get-Process -Name ffplay -ErrorAction SilentlyContinue
 foreach ($process in $ps) {
     Stop-Process -Id $process.Id
 }
-$arg = "-fflags nobuffer -probesize 32 -f lavfi -i `"amovie='udp\:\/\/224.1.1.1\:10001',showvolume=f=0:w=720:dm=1:h=10:s=1:p=1`" -top 405 -left 1200 -alwaysontop -noborder -hide_banner"
+$arg = "-fflags nobuffer -analyzeduration 500000 -f lavfi -i `"amovie='udp\:\/\/224.1.1.1\:10001',showvolume=f=0:w=720:dm=1:h=10:s=1:p=1`" -top 405 -left 1200 -alwaysontop -noborder -hide_banner"
 Start-Process -FilePath $PSScriptRoot\ffplay.exe -ArgumentList $arg -WindowStyle Minimized
-$arg = "-fflags nobuffer -probesize 32 -i udp://224.1.1.1:10001 -top 0 -left 1200 -x 720 -alwaysontop -noborder -hide_banner"
+$arg = "-fflags nobuffer -analyzeduration 500000 -i udp://224.1.1.1:10001 -top 0 -left 1200 -x 720 -alwaysontop -noborder -hide_banner"
 Start-Process -FilePath $PSScriptRoot\ffplay.exe -ArgumentList $arg -WindowStyle Minimized
-
-#Start-Sleep -Milliseconds 2000
 
 # Start bmd_h264_cat
 $dir = "C:\Data"
